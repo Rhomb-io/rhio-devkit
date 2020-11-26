647c553a045a3c4680a60dd5e3ed7b5c9dc682e4
jose1698 <jmarti@rhomb.io>
2020-11-26 09:16:23 +0100

    test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 #include <unity.h>
 
-#include "test_puf.h"
-#include "test_state-machine.h"
+//#include "test_puf.h"
+//#include "test_state-machine.h"
+
+#include "test_flash.h"
 
 void setup() {
   // NOTE!!! Wait for >2 secs
   // if board doesn't support software reset via Serial.DTR/RTS
   delay(2000);
-  pinMode(9, OUTPUT);
+  // pinMode(9, OUTPUT);
 
   UNITY_BEGIN();
-  state_test::init();
-  puf_test::init();
+  // state_test::init();
+  // puf_test::init();
+  flash_test::init();
   UNITY_END();
 }
 
